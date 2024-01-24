de847a5e26aff72fab4321c806d1021e1221b71f
ruialves7 <64751470+ruialves7@users.noreply.github.com>
2024-01-24 19:06:04 +0000

    Update clEnqueueCopyBuffer.c

--- code/rOpenCL_driver/lib/CL/clEnqueueCopyBuffer.c
+++ code/rOpenCL_driver/lib/CL/clEnqueueCopyBuffer.c
@@ -8,7 +8,7 @@ POname(clEnqueueCopyBuffer)(cl_command_queue command_queue,cl_mem src_buffer, cl
     char id = 0x1D;
     struct sockaddr_in addr;
     void * buffer_data_request = NULL,  *header = NULL,*ptr_ = NULL;
-    int fd = 0, size_buffer_data_request = 0, size_buffer_data_reply = 0, offset_buffer = 0;
+    int fd = 0; size_t size_buffer_data_request = 0, size_buffer_data_reply = 0; int offset_buffer = 0;
     ptr_ = lookup_object(command_queue);
     cl_opencl_object * obj = NULL;
     if (ptr_  != NULL) 
@@ -81,8 +81,8 @@ POname(clEnqueueCopyBuffer)(cl_command_queue command_queue,cl_mem src_buffer, cl
     _ccl_memcpy(buffer_data_request, &id, sizeof (char), &offset_buffer);
     buffer_data_request += sizeof (char);
 
-    _ccl_memcpy(buffer_data_request, &size_buffer_data_request, sizeof (int), &offset_buffer);
-    buffer_data_request += sizeof (int);
+    _ccl_memcpy(buffer_data_request, &size_buffer_data_request, sizeof (size_t), &offset_buffer);
+    buffer_data_request += sizeof (size_t);
 
 #endif
 
